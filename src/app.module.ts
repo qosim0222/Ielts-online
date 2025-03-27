ba3901136f83af5f08349e830266bc4875c08c41
qosim0222 <qosimxushvaxtov78@gmail.com>
2025-03-27 13:53:16 +0500

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UploadModule } from './upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [UploadModule, ServeStaticModule.forRoot({
+    rootPath:join(__dirname, '..', 'uploads'),
+    serveRoot:'/uploads'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
